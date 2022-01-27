6af55a6554010e6753bb9a276dd4da169f619ef7
wolfgang <wolfgang.f_dantas@posteo.de>
2022-01-27 13:17:08 +0100

    Temporal preprocessing * Remove all rows that have negative trip duration.

--- src/temporal_preprocessing.py
+++ src/temporal_preprocessing.py
@@ -4,6 +4,7 @@ def temporal_preprocessing(df_taxi_data):
     '''
     Add temporal information to the dataset:
         - Convert columns pickup_datetime, dropoff_datetime to type datetime64[ns].
+        - Remove all rows that have negative trip duration.
         - Add column trip_duration       (datetime64[ns]): Duration of each trip in datetime64[ns] format.
         - Add column trip_duration_min   (float)         : Duration of each trip in minutes.
         - Add column pickup_minute       (int, 0 ... 59) : The minute within the hour the passenger was picked up.
@@ -44,6 +45,9 @@ def temporal_preprocessing(df_taxi_data):
     
     # Extract minute from timestamp
     df_taxi_data['pickup_minute'] = df_taxi_data['pickup_datetime'].dt.minute
+    
+    # Remove negative trip duration
+    df_taxi_data = df_taxi_data[df_taxi_data['trip_duration_minutes'] > 0]
     
     # DEBUG: Check data types after all conversions.
     #df_taxi_data.info()
