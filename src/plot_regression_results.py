e3132a7fff9ff174172e8cdaa367d0e1822ea3f6
wolfgang <wolfgang.f_dantas@posteo.de>
2022-01-30 22:54:37 +0100

    Regression scripts.

---
+++ src/plot_regression_results.py
@@ -0,0 +1,32 @@
+def plot_regression_results(y_test,y_pred,weights):
+    '''Produces three plots to analyze the results of linear regression:
+        -True vs predicted
+        -Raw residual histogram
+        -Weight histogram
+        
+    Inputs:
+        y_test: (n_observations,) numpy array with true values
+        y_pred: (n_observations,) numpy array with predicted values
+        weights: (n_weights) numpy array with regression weights'''
+    
+    print('MSE: ', mean_squared_error(y_test,y_pred))
+    print('r^2: ', r2_score(y_test,y_pred))
+    
+    fig,ax = plt.subplots(1,3,figsize=(9,3))
+    #predicted vs true
+    ax[0].scatter(y_test,y_pred)
+    ax[0].set_title('True vs. Predicted')
+    ax[0].set_xlabel('True %s' % (target_clm))
+    ax[0].set_ylabel('Predicted %s' % (target_clm))
+
+    #residuals
+    error = np.squeeze(np.array(y_test)) - np.squeeze(np.array(y_pred))
+    ax[1].hist(np.array(error),bins=30)
+    ax[1].set_title('Raw residuals')
+    ax[1].set_xlabel('(true-predicted)')
+
+    #weight histogram
+    ax[2].hist(weights,bins='scott')
+    ax[2].set_title('weight histogram')
+
+    plt.tight_layout()
---
+++ src/sklearn_regression.py
@@ -0,0 +1,31 @@
+def sklearn_regression(X_test, X_train, y_train):
+    '''Computes OLS weights for linear regression without regularization using the sklearn library on the training set and 
+       returns weights and testset predictions.
+    
+       Inputs:
+         X_test: (n_observations, n_features), numpy array with predictor values of the test set 
+         X_train: (n_observations, n_features), numpy array with predictor values of the training set
+         y_train: (n_observations,) numpy array with true target values for the training set
+         
+       Outputs:
+         weights: The weight vector for the regerssion model including the offset
+         y_pred: The predictions on the TEST set
+          
+         
+       Note:
+         The sklearn library automatically takes care of adding a column for the offset.     
+    
+    '''
+    
+    # Instantiate LinearRegression object
+    lm = linear_model.LinearRegression()
+    # Fit linear model to the training data
+    lm.fit(X_train, y_train)
+    
+    # Obtain the weights estimated
+    weights = lm.coef_
+    
+    # Make prediction using the trained model
+    y_pred = lm.predict(X_test)
+    
+    return weights, y_pred
