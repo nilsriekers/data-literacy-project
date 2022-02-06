d33fab93cfe8b92af04d487ba2b86c8294525a42
nilsriekers <nils@riekers.info>
2022-02-06 20:17:40 +0100

    changes in data loader

--- src/load_taxi_data.py
+++ src/load_taxi_data.py
@@ -37,8 +37,19 @@ def load_taxi_data(fleets=['yellow'], years=[2021], months=[1]):
                         url_data = fleet + '_tripdata_' + str(year) + '-' + str(month) + '.csv'
                     # Try to download file
                     try:
-                        print('Will download... ' + url_data)
-                        df = pd.read_csv(url_prefix + url_data, low_memory=False)
+                        print('Will download... ' + url_data) # DEBUG
+                        df = pd.read_csv(url_prefix + url_data, encoding= 'ISO-8859–1', index_col=False, low_memory=False)
+                        # Make sure that all column names are consistant
+                        df.columns = df.columns.str.lower()
+                        # in old datasets there are coordinates instead of zone id's
+                        if ("pickup_longitude" in df.columns) or ("Pickup_longitude" in df.columns):
+                            # placeholder values
+                            df["PULocationID"] = int("-1")
+                            df["DOLocationID"] = int("-1")
+                        # Rename column
+                        df.rename(columns={'pulocationid':'PULocationID'}, inplace=True)
+                        # Rename column
+                        df.rename(columns={'dolocationid':'DOLocationID'}, inplace=True)
                     except HTTPError:
                         print('ERROR: There is no data available for fleet={}, years={}, months={}!'.format(fleet, years, months))
                         continue
@@ -50,8 +61,17 @@ def load_taxi_data(fleets=['yellow'], years=[2021], months=[1]):
                     if fleet == 'green':
                         # Features that all Green fleet data sets have in common
                         features = ['lpep_pickup_datetime', 'lpep_dropoff_datetime', 'PULocationID', 'DOLocationID']
-                    # Set FHV dataframe columns
-                    if fleet == 'fhv':
+                    # Set FHV(HV) dataframe columns
+                    if fleet == 'fhv' or fleet == 'fhvhv':
+                        # rename fhvhv fleet to fhv
+                        fleet = 'fhv'
+                        if ("pickup_date" in df.columns):
+                            # placeholder values
+                            df["dropoff_datetime"] = int("-1")
+                            df["PULocationID"] = int("-1")
+                            df["DOLocationID"] = int("-1")
+                            # rename column
+                            df = df.rename(columns={"pickup_date": "pickup_datetime"})
                         # Features that all FHV data sets have in common
                         features = ['pickup_datetime', 'dropoff_datetime', 'PULocationID', 'DOLocationID']
                     # Create dataframe => Only use relevant features and drop rest
@@ -80,7 +100,7 @@ def load_taxi_data(fleets=['yellow'], years=[2021], months=[1]):
                     # Try to download file
                     try:
                         print('Will download... ' + url_data)
-                        df = pd.read_csv(url_prefix + url_data, low_memory=False)
+                        df = pd.read_csv(url_prefix + url_data, encoding= 'ISO-8859–1', low_memory=False)
                     except HTTPError:
                         print('ERROR: There is no data available for fleet={}, years={}, months={}!'.format(fleet, years, months))
                         continue
@@ -135,7 +155,7 @@ def load_taxi_data(fleets=['yellow'], years=[2021], months=[1]):
                 # Try to download file
                 try:
                     print('Will download... ' + url_data) # DEBUG
-                    df = pd.read_csv(url_prefix + url_data, low_memory=False)
+                    df = pd.read_csv(url_prefix + url_data, encoding= 'ISO-8859–1', low_memory=False)
                 except HTTPError:
                     print('ERROR: There is no data available for fleet={}, years={}, months={}!'.format(fleet, years, months))
                     continue
@@ -149,6 +169,12 @@ def load_taxi_data(fleets=['yellow'], years=[2021], months=[1]):
                     features = ['lpep_pickup_datetime', 'lpep_dropoff_datetime', 'PULocationID', 'DOLocationID']
                 # Set FHV dataframe columns
                 if fleet == 'fhv':
+                    # Make sure that all column names are consistant
+                    df.columns = df.columns.str.lower()
+                    # Rename column
+                    df.rename(columns={'pulocationid':'PULocationID'}, inplace=True)
+                    # Rename column
+                    df.rename(columns={'dolocationid':'DOLocationID'}, inplace=True)
                     # Features that all FHV data sets have in common
                     features = ['pickup_datetime', 'dropoff_datetime', 'PULocationID', 'DOLocationID']
                 # Create dataframe => Only use relevant features and drop rest
